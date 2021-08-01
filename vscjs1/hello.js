3da1106e4848ea8eac8dea1bd36b16a8bab1326f
wxwpxh <15305732366@189.cn>
2021-08-01 19:45:35 +0800

    js-update

--- vscjs1/hello.js
+++ vscjs1/hello.js
@@ -1,23 +1,12 @@
-var fs = require("fs");
-var data = '';
+function hello() {
+	var a={};
+	var b=a;
 
-// 创建可读流
-var readerStream = fs.createReadStream('input.txt');
+	a.a=10;
+	b.b=12
 
-// 设置编码为 utf8。
-readerStream.setEncoding('UTF8');
-
-// 处理流事件 --> data, end, and error
-readerStream.on('data', function(chunk) {
-   data += chunk;
-});
-
-readerStream.on('end',function(){
-   console.log(data);
-});
-
-readerStream.on('error', function(err){
-   console.log(err.stack);
-});
-
-console.log("程序执行完毕");
+	console.log(a);
+	console.log(a);
+	console.log('Hello World!');
+}
+hello();
